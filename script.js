575084db6d245215629ef29dae9a974424442275
danish0501 <alidanish373@gmail.com>
2023-01-11 11:46:21 +0530

    Completed

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
-//your code here
+let input = document.getElementById("input");
+let expression = "";
+function solve(e) {
+  if (e === "ans") {
+    //evaluate ans
+    let result = eval(expression);
+    console.log(result);
+    input.value = result;
+  } 
+  else if (e === "clear") {
+    expression = "";
+    input.value = "0";
+  } 
+  else {
+    expression += e;
+    console.log(expression);
+    input.value = expression;
+  }
+}
